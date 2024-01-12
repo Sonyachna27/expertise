d3d31a80e2caf4415fba7148ff69bc1211fefcc5
Sonyachn27 <116268317+Sonyachn27@users.noreply.github.com>
2024-01-12 12:43:02 +0200

    add mobile style

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,3 +1,12 @@
+const menuBtn = document.querySelector('.menuBtns');
+const htmlElement = document.querySelector('html');
+const navLinks = document.querySelectorAll('nav a');
+  menuBtn.addEventListener('click' , () => document.querySelector('html').classList.toggle('open'));
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        htmlElement.classList.remove('open');
+    });
+});
 const accordionItems = document.querySelectorAll('.faq__container-accord-item');
 
     accordionItems.forEach(item => {
